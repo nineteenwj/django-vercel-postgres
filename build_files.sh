c8495e3bb0e0b93d29227a22d96de81335180520
nineteenwj <46552549+nineteenwj@users.noreply.github.com>
2023-07-04 23:15:58 +0800

    Create build_files.sh

---
+++ build_files.sh
@@ -0,0 +1,7 @@
+echo " Build Start"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+
+python3.9 manage.py collectstatic --noinput --clear
+echo " Build End" 
