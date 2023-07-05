dbf20cb265cc0fbed7dff79d33ad4c7fc02450ec
nineteenwj <46552549+nineteenwj@users.noreply.github.com>
2023-07-05 10:08:54 +0800

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 echo " Build Start"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py makemigrations
-python3.10 manage.py migrate
+python -V
+python -m pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
 
 echo " Build End" 
