7a792d788a68eae7e8c7cb47ce77d6dccb3d6e7e
nineteenwj <46552549+nineteenwj@users.noreply.github.com>
2023-07-05 11:04:06 +0800

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,4 +4,6 @@ python3.9 -m pip install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 
+python3.9 manage.py collectstatic --noinput --clear
+
 echo " Build End" 
